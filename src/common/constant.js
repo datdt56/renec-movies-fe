c757fb217f53a9fb936d477b5ccb0c00589711b7
datcf <datdt@classfunc.com>
2023-05-16 22:19:16 +0700

    update WS URL

--- src/common/constant.js
+++ src/common/constant.js
@@ -2,4 +2,4 @@ const IS_PROD = process.env.NEXT_PUBLIC_ENV === 'production'
 export const API_KEY = 'AIzaSyCuhITB31itgU5XFRcZoUZlbo2risU4YKo'
 export const API_URL = IS_PROD ? 'https://renec-movies-be.onrender.com' : 'http://localhost:8080'
 
-export const WEBSOCKET_URL = IS_PROD ? 'wss://renec-movies-be.onrender.com' : 'ws://localhost:8000'
+export const WEBSOCKET_URL = IS_PROD ? 'wss://renec-movies-be.onrender.com' : 'ws://localhost:8080'
